887376d7880105a57d48e2f9e449f0427fcacf25
Alibaba Ibadullayev <ali.ibadullayev.02@gmail.com>
2022-09-11 17:05:13 +0300

    done

--- yandex_contest/contest_2B/src/hw_1/E.java
+++ yandex_contest/contest_2B/src/hw_1/E.java
@@ -1,30 +1,29 @@
-import java.util.*;
+package hw_1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class E {
 
+    public static void solution() {
         Scanner scan = new Scanner(System.in);
         int d = scan.nextInt();
         int x = scan.nextInt();
-        int y= scan.nextInt();
-        if (checkTriangle(d, x, y)){
+        int y = scan.nextInt();
+        if (checkTriangle(d, x, y)) {
             System.out.println(0);
-        }
-        else {
-            double distA = findDist(0,0,x,y);
-            double distB = findDist(d,0,x,y);
-            double distC = findDist(0,d,x,y);
+        } else {
+            double distA = findDist(0, 0, x, y);
+            double distB = findDist(d, 0, x, y);
+            double distC = findDist(0, d, x, y);
             if (distA <= distB && distA <= distC)
                 System.out.println(1);
-            else if (distB<=distC && distB <= distA)
+            else if (distB <= distC && distB <= distA)
                 System.out.println(2);
             else
                 System.out.println(3);
         }
 
     }
-
     private static double findDist(int x0, int y0, int x, int y) {
         return Math.sqrt(Math.pow(x-x0,2)+Math.pow(y-y0,2));
     }
@@ -32,6 +31,5 @@ public class Main {
     private static boolean checkTriangle(int d, int x, int y) {
         return x>=0 && y>=0 && y<=-x+d;
     }
-
 
 }
---
+++ yandex_contest/contest_2B/src/hw_2/Main.java
@@ -0,0 +1,36 @@
+package hw_2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        SortedMap<Integer, Integer> map = new TreeMap<>();
+//        List<Integer> list = new ArrayList<>();
+        int num;
+        while ((num = scan.nextInt()) != 0){
+//            list.add(num);
+            if(!map.containsKey(num))
+                map.put(num, 1);
+            else
+                map.replace(num, map.get(num)+1);
+
+
+        }
+        System.out.println(map.get(map.lastKey()));
+//        list = list.stream().sorted().collect(Collectors.toList());
+//        int max = list.get(list.size()-1);
+//        System.out.println(list.stream().filter((a)->a==max).count());
+//        int max = list.get(0);
+//        for (int i = 1; i < list.size(); i++){
+//            max = Integer.max(max, list.get(i));
+//        }
+//        int res = 0;
+//        for (int i = 0; i < list.size(); i++){
+//            if (list.get(i) == max)
+//                res++;
+//        }
+//        System.out.println(res);
+    }
+}
